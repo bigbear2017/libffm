10a549b24e5d98b04fbd14a219c3f201a2bb8ae0
Cao Nannan <caonannan@wifi.com>
2018-02-11 14:50:48 +0800

    fix ffm server

--- ffm-server.cpp
+++ ffm-server.cpp
@@ -1,175 +1,63 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-#include <thrift/concurrency/ThreadManager.h>
-#include <thrift/concurrency/PlatformThreadFactory.h>
+#include "gen-cpp/FFMPredictor.h"
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
-#include <thrift/server/TThreadPoolServer.h>
-#include <thrift/server/TThreadedServer.h>
 #include <thrift/transport/TServerSocket.h>
-#include <thrift/transport/TSocket.h>
-#include <thrift/transport/TTransportUtils.h>
-#include <thrift/TToString.h>
+#include <thrift/transport/TBufferTransports.h>
 
-#include <boost/make_shared.hpp>
+#include "ffm.h"
 
-#include <iostream>
-#include <stdexcept>
-#include <sstream>
+using namespace ::apache::thrift;
+using namespace ::apache::thrift::protocol;
+using namespace ::apache::thrift::transport;
+using namespace ::apache::thrift::server;
 
-#include "../gen-cpp/Calculator.h"
+using boost::shared_ptr;
 
 using namespace std;
-using namespace apache::thrift;
-using namespace apache::thrift::concurrency;
-using namespace apache::thrift::protocol;
-using namespace apache::thrift::transport;
-using namespace apache::thrift::server;
+using namespace ffm;
 
-using namespace tutorial;
-using namespace shared;
-
-class FFMPredictorHandler: public FFMPredictorIf {
-public:
-  FFMPredictorHandler() {}
-
-  void ping() { cout << "ping()" << endl; }
-
-  double predict() {
-    cout << "calculate(" << logid << ", " << work << ")" << endl;
-    int32_t val;
-
-    switch (work.op) {
-    case Operation::ADD:
-      val = work.num1 + work.num2;
-      break;
-    case Operation::SUBTRACT:
-      val = work.num1 - work.num2;
-      break;
-    case Operation::MULTIPLY:
-      val = work.num1 * work.num2;
-      break;
-    case Operation::DIVIDE:
-      if (work.num2 == 0) {
-        InvalidOperation io;
-        io.whatOp = work.op;
-        io.why = "Cannot divide by 0";
-        throw io;
-      }
-      val = work.num1 / work.num2;
-      break;
-    default:
-      InvalidOperation io;
-      io.whatOp = work.op;
-      io.why = "Invalid Operation";
-      throw io;
+class FFMPredictorHandler : virtual public FFMPredictorIf {
+  private:
+  ffm_model model;
+  public:
+    FFMPredictorHandler(string & model_path) {
+      //load the ffm model
+      model = ffm_load_model(model_path);
     }
 
-    SharedStruct ss;
-    ss.key = logid;
-    ss.value = to_string(val);
+    void ping() {
+      printf("ping\n");
+    }
 
-    log[logid] = ss;
+    double predict(const std::vector<Node> & features) {
+      vector<ffm_node> x;
+      x.clear();
+      int size = features.size();
+      for( int i = 0; i < size; i ++ ) {
+        ffm_node N;
+        N.f = features[i].field;
+        N.j = features[i].feature;
+        N.v = features[i].value;
 
-    return val;
-  }
-
-  void getStruct(SharedStruct& ret, const int32_t logid) {
-    cout << "getStruct(" << logid << ")" << endl;
-    ret = log[logid];
-  }
-
-protected:
-  map<int32_t, SharedStruct> log;
+        x.push_back(N);
+      }
+      ffm_float y_bar = ffm_predict(x.data(), x.data()+x.size(), model);
+      printf("predict\n");
+      return y_bar;
+    }
 };
 
-/*
-  CalculatorIfFactory is code generated.
-  CalculatorCloneFactory is useful for getting access to the server side of the
-  transport.  It is also useful for making per-connection state.  Without this
-  CloneFactory, all connections will end up sharing the same handler instance.
-*/
-class CalculatorCloneFactory : virtual public CalculatorIfFactory {
- public:
-  virtual ~CalculatorCloneFactory() {}
-  virtual CalculatorIf* getHandler(const ::apache::thrift::TConnectionInfo& connInfo)
-  {
-    boost::shared_ptr<TSocket> sock = boost::dynamic_pointer_cast<TSocket>(connInfo.transport);
-    cout << "Incoming connection\n";
-    cout << "\tSocketInfo: "  << sock->getSocketInfo() << "\n";
-    cout << "\tPeerHost: "    << sock->getPeerHost() << "\n";
-    cout << "\tPeerAddress: " << sock->getPeerAddress() << "\n";
-    cout << "\tPeerPort: "    << sock->getPeerPort() << "\n";
-    return new CalculatorHandler;
-  }
-  virtual void releaseHandler( ::shared::SharedServiceIf* handler) {
-    delete handler;
-  }
-};
+int main(int argc, char **argv) {
+  int port = 9090;
+  std::string model_path = "/Users/caonannan/programs/github/libffm/model";
+  shared_ptr<FFMPredictorHandler> handler(new FFMPredictorHandler(model_path));
+  shared_ptr<TProcessor> processor(new FFMPredictorProcessor(handler));
+  shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
+  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
-int main() {
-  TThreadedServer server(
-    boost::make_shared<CalculatorProcessorFactory>(boost::make_shared<CalculatorCloneFactory>()),
-    boost::make_shared<TServerSocket>(9090), //port
-    boost::make_shared<TBufferedTransportFactory>(),
-    boost::make_shared<TBinaryProtocolFactory>());
-
-  /*
-  // if you don't need per-connection state, do the following instead
-  TThreadedServer server(
-    boost::make_shared<CalculatorProcessor>(boost::make_shared<CalculatorHandler>()),
-    boost::make_shared<TServerSocket>(9090), //port
-    boost::make_shared<TBufferedTransportFactory>(),
-    boost::make_shared<TBinaryProtocolFactory>());
-  */
-
-  /**
-   * Here are some alternate server types...
-
-  // This server only allows one connection at a time, but spawns no threads
-  TSimpleServer server(
-    boost::make_shared<CalculatorProcessor>(boost::make_shared<CalculatorHandler>()),
-    boost::make_shared<TServerSocket>(9090),
-    boost::make_shared<TBufferedTransportFactory>(),
-    boost::make_shared<TBinaryProtocolFactory>());
-
-  const int workerCount = 4;
-
-  boost::shared_ptr<ThreadManager> threadManager =
-    ThreadManager::newSimpleThreadManager(workerCount);
-  threadManager->threadFactory(
-    boost::make_shared<PlatformThreadFactory>());
-  threadManager->start();
-
-  // This server allows "workerCount" connection at a time, and reuses threads
-  TThreadPoolServer server(
-    boost::make_shared<CalculatorProcessorFactory>(boost::make_shared<CalculatorCloneFactory>()),
-    boost::make_shared<TServerSocket>(9090),
-    boost::make_shared<TBufferedTransportFactory>(),
-    boost::make_shared<TBinaryProtocolFactory>(),
-    threadManager);
-  */
-
-  cout << "Starting the server..." << endl;
+  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
   server.serve();
-  cout << "Done." << endl;
+  cout << "server starting ..." << endl;
   return 0;
 }
-
